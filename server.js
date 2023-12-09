8c5970555062339faec6147a4f1c889705e7947b
Jeff Wolfram <jeffwolfram@gmail.com>
2023-12-08 18:56:30 -0800

    Made changes to request.rest

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ const posts = [
 
 ]
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
 });
 
 app.get('/users' , (req, res) => {
@@ -60,9 +60,19 @@ app.post('/users/login', async (req, res) => {
     } catch {
         res.status(500).send()
     }
-    const username = req.body.username 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken })
+  
 })
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(3000)
