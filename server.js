335e4300f3d1f72e5d35ddfcc94841b5d90a84b2
Jeff Wolfram <jeffwolfram@gmail.com>
2024-01-13 17:23:52 -0800

    changed for production

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
 const initializePassport = require('./passport-config');
-const pool = require("./db.js"); 
+const pool = require('./prod-db.js'); 
 const e = require('express');
 const {isAdmin, isLead, isManager} = require('./roleMiddleware')
 initializePassport(passport);
